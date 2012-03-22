562e4b4f8e4247229ae1a88b2dded57b552497f4
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-21 19:53:29 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# ReadMe
+
+
+## Deployment
+
+```
+ey deploy
+```
+
+## Thanks
+
+The original scaffold for this application was created by [Engine Yard Rails Wizard](http://railswizard.engineyard.com).
+
+The project was created with the following recipes:
+
+* rails_basics
+* sidekiq
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SidekiqDemo::Application.config.secret_token = '445082daa3f8e3272394c4846de3bd2ada2541618603992afe7684f85c811d202aeb7a63241e3deae8100044e3995b2758125e11d5b36f40269a9d8c0844d47e'
